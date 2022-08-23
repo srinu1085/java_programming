f3f084a8fbb1643106701edece83eb397d5ff595
srinu1085 <84057026+srinu1085@users.noreply.github.com>
2022-08-23 20:52:43 +0530

    Create Adding_two_matrices.java

---
+++ Adding_two_matrices.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int a[][] = new int[10][10];
+	    int b[][] = new int[10][10];
+	    int c[][] = new int[10][10];
+	    Scanner sc = new Scanner(System.in);
+	    for(int i=0;i<2;i++){
+	        for(int j=0;j<2;j++){
+	            a[i][j] = sc.nextInt();
+	            b[i][j] = sc.nextInt();
+	        }
+	    }
+	    for(int i=0;i<2;i++){
+	        for(int j=0;j<2;j++){
+	            c[i][j]=a[i][j]+b[i][j];
+	        }
+	    }
+	    for(int i=0;i<2;i++){
+	        for(int j=0;j<2;j++){
+	            System.out.print(c[i][j]);
+	            System.out.print("  ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
